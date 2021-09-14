09fac954dfcbe233b9587bbc850bdaf9cca6551e
Olivier Rousseau-Guyot <olivier.rousseauguyot@shopify.com>
2021-09-14 10:27:24 -0400

    adds spin profile and function for vscode exts

--- spin/setup.sh
+++ spin/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/env bash
 source ~/dotfiles/utils.sh
 
-code --install-extension eamodio.gitlens
-code --install-extension fbosch.battery-indicator
-code --install-extension rid9.datetime
+_add_zsh_variant spin
+
+code-server --install-extension eamodio.gitlens
+code-server --install-extension fbosch.battery-indicator
+code-server --install-extension rid9.datetime
