b6cc47cc5be6ef51768e51028f2a0a73831fab92
Olivier Rousseau-Guyot <olivier.rousseauguyot@shopify.com>
2021-10-12 14:40:00 -0400

    alias add

--- spin/profile.sh
+++ spin/profile.sh
@@ -2,9 +2,13 @@
 
 vscode_setup() {
     code-server --install-extension eamodio.gitlens
-    code-server --install-extension fbosch.battery-indicator
-    code-server --install-extension rid9.datetime
+    # code-server --install-extension fbosch.battery-indicator
+    # code-server --install-extension rid9.datetime
 }
 
 alias wtest="yarn test --no-graphql"
 alias c="bin/rails c"
+
+git-update() {
+    stop && git fetch --all && git rebase origin/master && update && start
+}
