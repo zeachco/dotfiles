9509d3fa8055bd0cd370d4d841afc9f9d4aa1e65
Olivier Rousseau-Guyot <olivier.rousseauguyot@shopify.com>
2021-12-01 15:14:02 -0500

    adds alias for declarative-forms

--- osx/profile.sh
+++ osx/profile.sh
@@ -12,3 +12,9 @@ outside() {
   sleep 10
   outside
 }
+
+update_declarative_forms_in_playground() {
+  dev cd declarative-forms
+  yarn build && cp -r ./build ../declarative-forms-playground/node_modules/@shopify/declarative-forms/
+  cd -
+}
