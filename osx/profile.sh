b3165426b365ec83e3f5cf5922fd8f2f3ab7fa7c
Olivier Rousseau-Guyot <olivier.rousseauguyot@shopify.com>
2021-12-17 15:59:30 -0500

    removed touchbar hack

--- osx/profile.sh
+++ osx/profile.sh
@@ -5,14 +5,6 @@ xcode_kill() {
   xcode-select --install
 }
 
-brightness 1
-
-outside() {
-  brightness 1
-  sleep 10
-  outside
-}
-
 update_declarative_forms_in_playground() {
   dev cd declarative-forms
   yarn build && cp -r ./build ../declarative-forms-playground/node_modules/@shopify/declarative-forms/
