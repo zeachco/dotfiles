639952dc5e83c4269d2f640ed341d065f2de2c4c
Olivier Rousseau-Guyot <olivier.rousseauguyot@shopify.com>
2022-02-04 10:50:36 -0500

    remove brightness

--- osx/profile.sh
+++ osx/profile.sh
@@ -4,9 +4,3 @@ xcode_kill() {
   sudo rm -rf $(xcode-select -print-path)
   xcode-select --install
 }
-
-update_declarative_forms_in_playground() {
-  dev cd declarative-forms
-  yarn build && cp -r ./build ../declarative-forms-playground/node_modules/@shopify/declarative-forms/
-  cd -
-}
--- osx/setup.sh
+++ osx/setup.sh
@@ -2,5 +2,3 @@
 source ~/dotfiles/utils.sh
 
 _add_zsh_variant osx
-
-brew install brightness
