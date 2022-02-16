8ef84c2df047181ea235c21fa0bcbd1eab86114e
Olivier Rousseau-Guyot <olivier.rousseauguyot@shopify.com>
2022-02-16 14:22:50 -0500

    adds storybook

--- spin/profile.sh
+++ spin/profile.sh
@@ -12,3 +12,8 @@ alias c="bin/rails c"
 git-update() {
     stop && git fetch --all && git rebase origin/master && update && start
 }
+
+ssb() {
+    STORYBOOK_FOCUS=app/org-admin/Internal/sections/OrganizationIdentity/**/*.stories.@(mdx|tsx)
+    yarn storybook
+}
