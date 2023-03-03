d01e6c36fd58db13360a265756a767913ff76fe4
Olivier Rousseau-Guyot <zeachco@gmail.com>
2023-03-03 10:03:01 -0500

    conditional install neofetch osx

--- osx/setup.sh
+++ osx/setup.sh
@@ -1,11 +1,16 @@
 #!/bin/env bash
 source ~/dotfiles/utils.sh
 
-brew install neofetch
-
-_add_zsh_variant osx
+if [ $(command -v neofetch) ] ; then
+    echo "neofetch already installed"
+else
+    echo "Installing neofetch"
+    brew install neofetch
+fi
 
 FONT=VictorMono
 
 brew tap homebrew/cask-fonts &&
 brew install --cask font-$FONT-nerd-font
+
+_add_zsh_variant osx
