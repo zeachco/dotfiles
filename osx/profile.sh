cdbfe9142ec1196c12a2a11beb1269cb5e3feadb
Olivier Rousseau-Guyot <olivier.rousseauguyot@shopify.com>
2023-07-17 10:08:17 -0400

    Moved _set to utils and added bank alias

--- osx/profile.sh
+++ osx/profile.sh
@@ -4,3 +4,9 @@ xcode_kill() {
   sudo rm -rf $(xcode-select -print-path)
   xcode-select --install
 }
+
+function _set () {
+  alias $1="echo -e \" ~ \033[0;34m$2\033[0m\" && $2"
+}
+
+_set bank "spin up ~/sbanking.yml --wait"
