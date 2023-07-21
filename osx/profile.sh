e274573f7b5175458fb7fb531f715f454904ea7d
Olivier Rousseau-Guyot <olivier.rousseauguyot@shopify.com>
2023-07-21 10:27:41 -0400

    tmux reset script

--- osx/profile.sh
+++ osx/profile.sh
@@ -10,3 +10,23 @@ function _set () {
 }
 
 _set bank "spin up ~/sbanking.yml --wait"
+
+
+function refresh_web_deps() {
+  session_name="banking_refresh"
+
+  tmux new-session -d -s $session_name
+  tmux split-window -h
+
+  tmux send-keys -t $session_name:1.1 'cd ../shopify && git fetch --all && git reset --hard origin/main && update' C-m
+  tmux send-keys -t $session_name:1.2 'cd ../banking && git fetch --all && git reset --hard origin/main && update' C-m
+
+  tmux attach-session -t $session_name
+
+  wait
+
+  # Kill the session
+  tmux kill-session -t $session_name
+
+  cd ../web && git fetch --all && git rebase origin/main && update
+}
