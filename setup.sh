707220cbc586418cb81846d3667718d22f4e4845
Olivier <zeachco@gmail.com>
2024-02-07 01:36:06 -0500

    removed spin specifics

--- setup.sh
+++ setup.sh
@@ -13,9 +13,5 @@ esac
 
 install_profile "common"
 
-if [[ "$USER" == "spin" ]]; then
-    install_profile "spin"
-fi
-
 # note: the debian profile is also adapted to arch/manjaroo
 install_profile "$OS_DIR"
--- spin/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/env bash
-source ~/dotfiles/utils.sh
-
-script_install fzf "git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --completion --key-bindings --update-rc"
-script_install lvim "bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) --no-install-dependencies && sudo ln -s ~/.local/bin/lvim /usr/bin/lvim"
