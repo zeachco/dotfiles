cd882f5d0b430eeef926d5cfbef7acb42d9b6d2e
Olivier <zeachco@gmail.com>
2024-05-13 09:11:12 -0400

    removed deprecated commands

--- osx/profile.sh
+++ osx/profile.sh
@@ -10,14 +10,3 @@ xcode_reinstall() {
 _set () {
   alias $1="echo -e \" ~ \033[0;34m$2\033[0m\" && $2"
 }
-
-_set bank "spin up ~/sbanking.yml --wait"
-
-tableflip() {
-    echo "(╯°□°)╯︵ ┻━┻";
-    spin destroy --all
-    spin login
-    bank
-    spin code
-    say "`node -e "n=new Date,f=t=>Math.abs(Math.round((t)/60000)),e=new Date(n.toLocaleString().split(', ')[0]+', 17:00:00'),o=console.log('code is open.'+n>e?'You are over '+f(n-e)+' minutes, just go home':'You have '+f(e-n)+' minutes left today')"`"
-}
