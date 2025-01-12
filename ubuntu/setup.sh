e2b8d0e660b39213dcdfa84917cf83082c10573d
Olivier <zeachco@gmail.com>
2025-01-11 20:54:11 -0500

    simplified ubuntu setup and added devbox

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,25 +1,17 @@
 #!/bin/env sh
 source ~/dotfiles/utils.sh
 
-#script_install omakub "wget -qO- https://omakub.org/install | $SHELL"
+# Assuming Ubuntu is using Omakub
 
 install neofetch
 
 # tools
 install unzip
-install tmux
 install ifconfig net-tools
 install dig dnsutils
-install curl
 install htop
 install g++
 install make
-install gh github-cli
 install peek
-script_install ollama "curl -fsS https://ollama.com/install.sh | $SHELL"
-script_install mise "curl https://mise.run | $SHELL"
-mise use deno
-mise use rust
-mise use bun
-mise use node
-mise upgrade --bump
+
+script_install devbox "curl -fsSL https://get.jetify.com/devbox | $SHELL"
