ce02b4a3f2d4f5cfeabff680ba2a6d3222dbd788
Olivier Rousseau <zeachco@gmail.com>
2025-08-14 19:42:31 -0400

    fix: source files crashing the current terminal

--- common/profile.sh
+++ common/profile.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
-set -e
 
 export EDITOR="nvim"
 export SUDO_EDITOR="$EDITOR"
 
 DOT_DIR="$HOME/dotfiles"
 dotfiles_update() {
-    cd $DOT_DIR || exit 1
-    git fetch
-    git reset --hard origin/main
-    cd - || exit 1
+  cd $DOT_DIR || exit 1
+  git fetch
+  git reset --hard origin/main
+  cd - || exit 1
 
-    $SHELL "$DOT_DIR/setup.sh"
+  $SHELL "$DOT_DIR/setup.sh"
 }
 
 alias update_dotfiles="dotfiles_update"
 
 gcommits() {
-    if [ -z "$1" ];
-    then git log --format="%C(auto)%h (%s, %ad)" -n 20 | cat;
-    else git log --format="%H" -n $1 | cat;
-    fi
+  if [ -z "$1" ]; then
+    git log --format="%C(auto)%h (%s, %ad)" -n 20 | cat
+  else
+    git log --format="%H" -n $1 | cat
+  fi
 }
 
 use() {
@@ -74,42 +74,42 @@ killport() {
 }
 
 killname() {
-    for pid in $(ps -e | grep "$1" | awk '{print $1}'); do
-        process_name=$(ps -p "$pid" -o comm=)
-        echo "Are you sure you want to kill process $pid ($process_name)? [y/N]"
-        read response
-        if echo "$response" | grep -q '^[yY]\([eE][sS]\)\?$'; then
-            sudo kill -9 "$pid"
-            echo "Killed process $pid ($process_name)"
-        else
-            echo "Skipped process $pid ($process_name)"
-        fi
-    done
+  for pid in $(ps -e | grep "$1" | awk '{print $1}'); do
+    process_name=$(ps -p "$pid" -o comm=)
+    echo "Are you sure you want to kill process $pid ($process_name)? [y/N]"
+    read response
+    if echo "$response" | grep -q '^[yY]\([eE][sS]\)\?$'; then
+      sudo kill -9 "$pid"
+      echo "Killed process $pid ($process_name)"
+    else
+      echo "Skipped process $pid ($process_name)"
+    fi
+  done
 }
 
-ipp () {
-    dig +short myip.opendns.com @resolver1.opendns.com
+ipp() {
+  dig +short myip.opendns.com @resolver1.opendns.com
 }
 
-ipl () {
-    ifconfig | grep broadcast | awk '{print $2}'
+ipl() {
+  ifconfig | grep broadcast | awk '{print $2}'
 }
 
 node_admin() {
-    # allows node to run on admin ports such as 80 and 443
-    sudo setcap 'cap_net_bind_service=+ep' `which node`
-    echo "Changing max notify watcher from $(cat /proc/sys/fs/inotify/max_user_watches) to 524288 (max value)"
-    echo "fs.inotify.max_user_watches=524288" | sudo tee -a  /etc/sysctl.conf
+  # allows node to run on admin ports such as 80 and 443
+  sudo setcap 'cap_net_bind_service=+ep' $(which node)
+  echo "Changing max notify watcher from $(cat /proc/sys/fs/inotify/max_user_watches) to 524288 (max value)"
+  echo "fs.inotify.max_user_watches=524288" | sudo tee -a /etc/sysctl.conf
 }
 
 alias clone="bun ~/dotfiles/advanced/clone.ts"
 
 git_test() {
-    DEFAULT_FILTER="test.ts"
-    DEFAULT_RUNNER="npx jest --watch"
-    FILTER="${1:-$DEFAULT_FILTER}"
-    RUNNER="${2:-$DEFAULT_RUNNER}"
-    git diff origin/main --name-only | grep $FILTER |  fzf -m | xargs $RUNNER
+  DEFAULT_FILTER="test.ts"
+  DEFAULT_RUNNER="npx jest --watch"
+  FILTER="${1:-$DEFAULT_FILTER}"
+  RUNNER="${2:-$DEFAULT_RUNNER}"
+  git diff origin/main --name-only | grep $FILTER | fzf -m | xargs $RUNNER
 }
 _set gt "git_test"
 
@@ -122,17 +122,19 @@ denode() {
 }
 
 codeai() {
-    file=$1
-    prompt=$2
-    echo "starting AI analysis for $file..."
-    fullprompt="file: '$file':\n \`\`\`\n$(cat $file)\n\n\`\`\`\n\nplease rewrite its content to satisfy the following: $prompt\n\n"
-    echo "$fullprompt" | ollama run codellama:13b > "$file" & echo "to stop: \n kill $!";
+  file=$1
+  prompt=$2
+  echo "starting AI analysis for $file..."
+  fullprompt="file: '$file':\n \`\`\`\n$(cat $file)\n\n\`\`\`\n\nplease rewrite its content to satisfy the following: $prompt\n\n"
+  echo "$fullprompt" | ollama run codellama:13b >"$file" &
+  echo "to stop: \n kill $!"
 }
 
 speakai() {
-    prompt=$1
-    echo "starting AI..."
-    echo "$prompt" | ollama run mistral | espeak -s150 -g4 -p55 -a 200 & echo "to stop: \n kill $!";
+  prompt=$1
+  echo "starting AI..."
+  echo "$prompt" | ollama run mistral | espeak -s150 -g4 -p55 -a 200 &
+  echo "to stop: \n kill $!"
 }
 
 if [ "${SHELL##*/}" = "zsh" ]; then
@@ -147,30 +149,33 @@ ai() {
 }
 
 pie_score() {
-    echo "Generate a PIE score by listing 3 score for Physical, Intellectual and Emotional, each line starts with the name of the score followed by 'is <score>, because <make up a casual reason matching the category>'" | ollama run mistral
+  echo "Generate a PIE score by listing 3 score for Physical, Intellectual and Emotional, each line starts with the name of the score followed by 'is <score>, because <make up a casual reason matching the category>'" | ollama run mistral
 }
 
 # replace normal to call hook after the command
-# cd() {
-#   builtin cd "$@" || return
-#   check_for_devbox
-# }
-#
-# check_for_devbox() {
-#   if [[ -f "devbox.json" ]]; then
-#     if [[ -n "$DEVBOX_WORKING_DIR" && "$DEVBOX_WORKING_DIR" != "$(pwd)" ]]; then
-#       echo "switching to devbox shell ($(pwd))..."
-#       eval "$(devbox shellenv --preserve-path-stack -c "/Users/olivierr/dev/brownbags")" && hash -r
-#     elif [[ -z "$DEVBOX_WORKING_DIR" ]]; then
-#       echo "Found devbox.json. Entering devbox shell..."
-#       which devbox > /dev/null || curl -fsSL https://get.jetify.com/devbox | bash
-#       export DEVBOX_WORKING_DIR="$(pwd)"
-#       devbox shell
-#     fi
-#   fi
-# }
-#
-# # also call on shell open for when you split your terminal on an existing devbox path
-# # remove this line if you find this behavior too intrusive
-# check_for_devbox
+cd() {
+  builtin cd "$@" || return
+  check_for_devbox
+}
 
+check_for_devbox() {
+  if [[ -f "devbox.json" ]]; then
+    if [[ -n "$DEVBOX_WORKING_DIR" && "$DEVBOX_WORKING_DIR" != "$(pwd)" ]]; then
+      echo "switching to devbox shell ($(pwd))..."
+      eval "$(devbox shellenv --preserve-path-stack -c "/Users/olivierr/dev/brownbags")" && hash -r
+    elif [[ -z "$DEVBOX_WORKING_DIR" ]]; then
+      echo "Found devbox.json. Entering devbox shell..."
+      which devbox >/dev/null || curl -fsSL https://get.jetify.com/devbox | bash
+      export DEVBOX_WORKING_DIR="$(pwd)"
+      devbox shell
+    fi
+  fi
+}
+
+# also call on shell open for when you split your terminal on an existing devbox path
+# remove this line if you find this behavior too intrusive
+check_for_devbox
+
+# Fix for terminal closing on any error
+# Disable errexit mode that causes shell to exit on command errors
+set +e
