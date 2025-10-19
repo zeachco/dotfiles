2b024b76ae530ea63bca6240d8f86f45e62f392f
Olivier Rousseau <zeachc@gmail.com>
2025-10-19 14:25:48 -0400

    feat: adds zapt (fast apt search)

--- ubuntu/profile.sh
+++ ubuntu/profile.sh
@@ -10,3 +10,15 @@ wifi() {
 kde_restart() {
   sudo systemctl restart display-manager
 }
+
+zapt() {
+  package="$(
+    apt-cache pkgnames |
+      fzf -m --prompt="Enter Package Names: " \
+        --preview "apt show {} 2>/dev/null" \
+        --preview-window 'top:75%' |
+      tr "\n" " "
+  )"
+  [[ $package ]] && read -n1 -p "Install ${package}? [y/N]: " install
+  [[ "$install" == "y" ]] && sudo apt install $package
+}
