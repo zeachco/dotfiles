cde228e291f0215408484b52087262722094b7bf
Olivier Rousseau-Guyot <olivier.rousseau.guyot@stay22.com>
2025-11-27 20:32:24 -0500

    feat: added bordo theme

---
+++ themes/bordo/neovim.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "liviuschera/noctis",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "noctis_bordo",
+    },
+  },
+}
