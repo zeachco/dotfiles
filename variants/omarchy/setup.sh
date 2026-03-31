7f16d7470b16f2af9395d92129ab0596bb51faed
Olivier Rousseau <zeachco@gmail.com>
2026-03-31 00:41:57 -0400

    feat: adds hypr configs (omarchy)

--- variants/omarchy/setup.sh
+++ variants/omarchy/setup.sh
@@ -3,8 +3,9 @@ source "$HOME/dotfiles/utils.sh"
 
 install stow
 
-# Waybar config
+# Omarchy look and feel configs
 cd "$DOT_DIR"
 stow --restow waybar
+stow --restow hypr
 
 echo -e "${PASS}Omarchy setup complete!${NORM}"
