43ec5dc868aca1a4c31310dd9b0652d5e9a5bf2b
Olivier Rousseau <zeachco@gmail.com>
2026-08-12 16:08:59 -0400

    feat: vscode like multicursor for nvim

---
+++ configs/nvim/.config/nvim/lua/plugins/multicursor.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "mg979/vim-visual-multi",
+    branch = "master",
+    init = function()
+      vim.g.VM_maps = {
+        ["Find Under"] = "<C-d>",
+        ["Find Subword Under"] = "<C-d>",
+        ["Select All"] = "<C-S-l>",
+        ["Visual All"] = "<C-S-l>",
+      }
+    end,
+  },
+}
